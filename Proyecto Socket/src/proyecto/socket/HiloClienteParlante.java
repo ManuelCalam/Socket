e8cd6557bb2a16a3023c31c402454262774df2a7
YERP02 <105952970+YERP02@users.noreply.github.com>
2024-05-22 08:45:07 -0600

    Update HiloClienteParlante.java

--- Proyecto Socket/src/proyecto/socket/HiloClienteParlante.java
+++ Proyecto Socket/src/proyecto/socket/HiloClienteParlante.java
@@ -5,10 +5,43 @@
  */
 package proyecto.socket;
 
+import com.sun.istack.internal.logging.Logger;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+
 /**
  *
  * @author yahir
  */
-public class HiloClienteParlante {
-    
+public class HiloClienteParlante extends Thread {
+
+    protected Socket sk;
+    protected DataOutputStream dos;
+    protected DataInputStream dis;
+    private int id;
+
+    public HiloClienteParlante (int id) {
+        this.id = id;
+    }
+    @Override
+    public void run() {
+        try {
+            sk = new Socket ("127.0.0.1", 8080);
+            dos = new DataOutputStream(sk.getOutputStream());
+            dis = new DataInputStream(sk.getInputStream());
+            System. out.println (id + " envia saludo");
+            dos.writeUTF("hola");
+            String respuesta="";
+            respuesta = dis.readUTF();
+            System. out.println (id + " Servidor devuelve saludo: " + respuesta);
+            dis.close();
+            dos.close();
+            sk.close();
+        } catch (IOException ex) {
+            Logger.getLogger(HiloClienteParlante.class.getName()).log (Level.SEVERE, null, ex);
+        }
+    }
 }
