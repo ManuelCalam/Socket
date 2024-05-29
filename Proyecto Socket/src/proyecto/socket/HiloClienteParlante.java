465e709750c69bbeffac4edfa6f89a05d7f82246
ManuelCalam <jmanuelc369@gmail.com>
2024-05-28 23:35:55 -0600

    Cliente 1 Agrega datos

--- Proyecto Socket/src/proyecto/socket/HiloClienteParlante.java
+++ Proyecto Socket/src/proyecto/socket/HiloClienteParlante.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto.socket;
 
 import java.io.DataInputStream;
@@ -13,80 +8,65 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author yahir
- */
 public class HiloClienteParlante extends Thread {
-    
 
-    public boolean client0finished = false;
+    private static boolean client0finished = false;
+    private int id;
     protected Socket sk;
     protected DataOutputStream dos;
     protected DataInputStream dis;
-    private int id;
 
-    public HiloClienteParlante (int id) {
+    public HiloClienteParlante(int id) {
         this.id = id;
     }
+
     @Override
-    public synchronized void run() {
-        while(!client0finished && id == 1){
+    public void run() {
+        synchronized (HiloClienteParlante.class) {
+            while (id == 1 && !client0finished) {
+                try {
+                    HiloClienteParlante.class.wait();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(HiloClienteParlante.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+            Scanner scanner = new Scanner(System.in);
+            String respuesta = "";
+
             try {
-                wait();
-            } catch (InterruptedException ex) {
+                sk = new Socket("25.39.100.34", 1000);
+                dos = new DataOutputStream(sk.getOutputStream());
+                dis = new DataInputStream(sk.getInputStream());
+                System.out.println(id + " envia saludo");
+
+                System.out.print("Cliente " + id + " - Ingresa un número: ");
+                double numero1 = scanner.nextDouble();
+
+                dos.writeUTF(String.valueOf(numero1));
+                respuesta = dis.readUTF();
+                System.out.println("Cliente " + id + " - Servidor devuelve saludo: " + respuesta);
+
+                scanner.nextLine();
+
+                System.out.print("Cliente " + id + " - Ingresa la operación (+, -, *, /, %): ");
+                String operacion = scanner.next();
+                
+                dos.writeUTF(operacion);
+                respuesta = dis.readUTF();
+                System.out.println("Cliente " + id + " - Servidor devuelve saludo: " + respuesta);
+
+                dis.close();
+                dos.close();
+                sk.close();
+            } catch (IOException ex) {
                 Logger.getLogger(HiloClienteParlante.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }
-        
-        Scanner scanner = new Scanner(System.in);
-        
 
-     
-        String respuesta="";
-
-        
-        try {
-            sk = new Socket ("25.39.100.34", 1000);
-            dos = new DataOutputStream(sk.getOutputStream());
-            dis = new DataInputStream(sk.getInputStream());
-            System. out.println (id + " envia saludo");
-            //dos.writeUTF("hola");
-            
-            
-            System.out.print("Cliente " + id + " - Ingresa un número: ");
-            double numero1 = scanner.nextDouble();
-            
-            dos.writeUTF(String.valueOf(numero1));
-            respuesta = dis.readUTF();
-            System. out.println ("Cliente " + id + " - Servidor devuelve saludo: " + respuesta);
-            
-            scanner.nextLine(); 
-            //notify();
-            
-            
-            System.out.print("Cliente " +id + " - Ingresa la operación (+, -, *, /, %): ");
-            double operacion = scanner.nextDouble();
-            
-            dos.writeUTF(String.valueOf(operacion));
-            respuesta = dis.readUTF();
-            System. out.println ("Cliente " + id + " - Servidor devuelve saludo: " + respuesta);
-            
-            scanner.nextLine(); 
-            
-            
-            
-                
-            client0finished = true;
-            
-            
-            dis.close();
-            dos.close();
-            sk.close();
-        } catch (IOException ex) {
-            Logger.getLogger(HiloClienteParlante.class.getName()).log (Level.SEVERE, null, ex);
+            if (id == 0) {
+                client0finished = true;
+                HiloClienteParlante.class.notifyAll();
+            }
         }
-        
-        notifyAll();
     }
 }
